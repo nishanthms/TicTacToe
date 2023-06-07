dc88de53b814d92c6e715110255fae162609b6ef
Nishanth M S <nishanthms20@gmail.com>
2023-06-07 09:33:55 +0530

    Added some padding around Header text

--- client/src/Component/Header.tsx
+++ client/src/Component/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
             <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
                 <div className="flex items-center">
                     <img width="100" height="100" src="HeaderIcon.jpeg" alt="TIC TAC TOE" />
-                    <span className="text-lg font-bold">TIC TAC TOE</span>
+                    <span className="text-lg font-bold  p-4">TIC TAC TOE</span>
                 </div>
                 {/* <div>
                     <button className="mr-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded">
